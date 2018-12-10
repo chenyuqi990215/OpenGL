41b121ecb624e1b701ca12ec0775009d3c457a74
chenyuqi990215 <42923882+chenyuqi990215@users.noreply.github.com>
2018-12-10 15:12:52 +0800

    Update light.cpp

--- library/light.cpp
+++ library/light.cpp
@@ -2,51 +2,11 @@
 #include <GL/glew.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <windows.h>
-#include <wingdi.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glaux.h>
+#include "light.h"
 
-class Light
-{
-    public:
-        enum LightType{SPOT_LIGHT,POINT_LIGHT,DIRECTIONAL_LIGHT};
-		enum Mode{DEFAULT,DEFINATION};
-		Light(GLenum light,LightType type,Mode mode);
-		void setMatSpecular(GLfloat x,GLfloat y,GLfloat z);
-		void setMatShininess(GLfloat shininess);
-		void setLightPosition(GLfloat x,GLfloat y,GLfloat z);
-		void setLightAmbient(GLfloat r,GLfloat g,GLfloat b);
-		void setLightDiffuse(GLfloat r,GLfloat g,GLfloat b);
-		void setLightSpecular(GLfloat x,GLfloat y,GLfloat z);
-		void setSpotDirection(GLfloat x,GLfloat y,GLfloat z);
-		void setLightModelAmbient(GLfloat x,GLfloat y,GLfloat z);
-		void setAngle(GLfloat angle);
-		void init();
-		void enable();
-		void disable();
-	private:
-		GLenum light;
-		LightType type;
-		Mode mode;
-		void initSpotLight();
-		void initPointLight();
-		void initDirecitonalLight();
-		GLfloat matSpecular[4];
-		GLfloat matShininess[1];
-		GLfloat lightPosition[4];
-		GLfloat lightAmbient[4];
-		GLfloat lightDiffuse[4];
-		GLfloat lightSpecular[4];
-		GLfloat spotDirection[4];
-		GLfloat lightModelAmbient[4];
-		GLfloat angle;
-		void setDefault();
-		void setValue4(GLfloat* arr,GLfloat a,GLfloat b,GLfloat c,GLfloat d);
-
-};
 Light::Light(GLenum light,LightType type,Mode mode)
 {
 	this->light = light;
