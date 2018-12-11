dcfa73b0c0609df36a8ec1caca9bd58b2678bed2
chenyuqi990215 <42923882+chenyuqi990215@users.noreply.github.com>
2018-12-11 16:31:33 +0800

    Update loadtexture.h

--- library/loadtexture.h
+++ library/loadtexture.h
@@ -5,16 +5,20 @@
 #include <GL/glaux.h>
 #include <bits/stdc++.h>
 using namespace std;
+static map <string,GLuint> texturePool;
 class LoadTexture
 {
 	private:
 		string textureName;
 		GLuint textureID;
 	public:
+	    LoadTexture();
 		LoadTexture(string textureName);
+		void setTextureName(string textureName);
 		void load();
 		void init();
 		void enable();
+		bool hasLoad();
 };
 
 #endif // LOADTEXTURE_H_INCLUDED
