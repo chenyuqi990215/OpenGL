1a5abe1186d026f05359c0b6844e3c39014aa956
chenyuqi990215 <42923882+chenyuqi990215@users.noreply.github.com>
2018-12-11 16:30:01 +0800

    Update group.cpp

--- library/group.cpp
+++ library/group.cpp
@@ -7,15 +7,55 @@
 #include <GL/glaux.h>
 #include "group.h"
 #include "point.h"
+#include "loadtexture.h"
 Group::Group()
 {
 	this->s.clear();
 	this->textureName="";
 	this->hasTexture=false;
 }
-Group::Group(string textureName)
+void Group::updateTexture(string textureName)
 {
-	this->s.clear();
 	this->textureName=textureName;
 	this->hasTexture=true;
+	this->texture.LoadTexture::setTextureName(textureName);
+}
+void Group::drawGroup()
+{
+	if (this->hasTexture==false)
+	{
+		for (int i=0;i<this->s.size();i++)
+		{
+			glBegin(GL_TRIANGLES);
+			for (int j=0;j<3;j++)
+			{
+				glColor3f(1,1,0);
+				glVertex3f(this->s[i].v[j].x,this->s[i].v[j].y,this->s[i].v[j].z);
+			}
+			glEnd();
+		}
+	}
+	else
+	{
+	    if (!this->texture.hasLoad())
+        {
+            this->texture.load();
+            this->texture.init();
+            this->texture.enable();
+        }
+        else
+        {
+            this->texture.enable();
+        }
+		for (int i=0;i<this->s.size();i++)
+		{
+			glBegin(GL_TRIANGLES);
+			for (int j=0;j<3;j++)
+			{
+				glTexCoord2f(this->s[i].vt[j].x,this->s[i].vt[j].y);
+				glVertex3f(this->s[i].v[j].x,this->s[i].v[j].y,this->s[i].v[j].z);
+			}
+			glEnd();
+		}
+	}
 }
